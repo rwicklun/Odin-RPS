92445ed44d34cda637b1e6761d7d32ae2b1ab0cc
Richard Wicklund <51726996+rwicklun@users.noreply.github.com>
2023-04-06 08:24:35 -0700

    Replace console messages with UI functionality

--- js/script.js
+++ js/script.js
@@ -45,44 +45,59 @@ function playRound(playerChoice, computerChoice){
     return -1;
 
 }
-
-function game () {
+function rps () {
     let playerWins = 0;
     let computerWins = 0;
     let ties = 0;
-    let games = [];
-    for (let round = 1; round <= 5; round++) {
-        console.log(`Round ${round} start`);
-        let userInput = prompt("Please enter: Rock, Paper, or Scissors");
-        let computerInput = getComputerChoice();
-        games[round] = playRound(userInput, computerInput);
-        switch (games[round]) {
-            case 0:
-                ties++;
-                console.log(`It's a TIE! Nobody won today ${userInput} is equal to ${computerInput}`);
-                break;
-            case 1:
-                playerWins++;
-                console.log(`You won rond ${round}! ${userInput} beats ${computerInput}`)
-                break;
-            case 2:
-                computerWins++;
-                console.log(`You lost round ${round}! ${computerInput} beats ${userInput}`)
-                break;
-            default:
-                break;
-
-        }
-
-    }
-    if (playerWins > computerWins) {
-        console.log(`You won the game! \nYou won: ${playerWins} times. \nYou lost: ${computerWins} times. \nYou tied: ${ties} times`)
-    }
-    if (playerWins < computerWins) {
-        console.log(`You lost the game! \nYou won: ${playerWins} times. \nYou lost: ${computerWins} times. \nYou tied: ${ties} times`)
-    }
-    if (playerWins == computerWins) {
-        console.log(`You tied the game! \nYou won: ${playerWins} times. \nYou lost: ${computerWins} times. \nYou tied: ${ties} times`)
-    }
+    let round = 1;
+    document.querySelector('.round').textContent = `Round: ${round}`;
+    // buttons need to listen
+    const resetButton = document.querySelector('#reset');
+    resetButton.addEventListener('click', () => {
+        playerWins = 0;
+        computerWins = 0;
+        ties = 0;
+        round = 1;
+        document.querySelector('.scoreBoard > h2').textContent = `Score: ${playerWins}:${computerWins} Ties: ${ties}`;
+        document.querySelector('.round').textContent = `Round: ${round}`;
+        document.querySelector('.message').textContent = '';
+    });
+    const playerButtons = document.querySelectorAll('.userButtons > button');
+    playerButtons.forEach((button)=> {
+        button.addEventListener('click', (event) => {
+            if (playerWins >=5 || computerWins >=5) return;
+            console.log(`Round ${round} start!`);
+            let userInput = event.target.className;
+            let computerInput = getComputerChoice();
+            switch (playRound(userInput, computerInput)) {
+                case 0:
+                    ties++;
+                    console.log(`It's a TIE! Nobody won today ${userInput} is equal to ${computerInput}`);
+                    break;
+                case 1:
+                    playerWins++;
+                    console.log(`You won this round! ${userInput} beats ${computerInput}`)
+                    break;
+                case 2:
+                    computerWins++;
+                    console.log(`You lost this round! ${computerInput} beats ${userInput}`)
+                    break;
+                default:
+                    console.error('Magic has happend you should not be here.');
+                    break;
+            };
+            round++;
+            document.querySelector('.round').textContent = `Round: ${round}`;
+            document.querySelector('.scoreBoard > h2').textContent = `Score: ${playerWins}:${computerWins} Ties: ${ties}`;
+            if (playerWins >= 5) {
+                document.querySelector('.message').textContent = `You won the game!`;
+            }
+            if (computerWins >= 5) {
+                document.querySelector('.message').textContent = `You lost the game!`;
+            }
+            
+        });
+    });
 }
-game();
+rps();
+    
